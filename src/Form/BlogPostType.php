cf23188e6de239ad4ebd02bb3bf03ae9f04ef7f8
Kekee <kekekuza@gmail.com>
2022-10-27 12:03:18 +0300

    --

---
+++ src/Form/BlogPostType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\BlogPost;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlogPostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Title')
+            ->add('Description')
+            ->add('Content')
+            ->add('BlogCategory')
+            ->add('BlogTags')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => BlogPost::class,
+        ]);
+    }
+}
